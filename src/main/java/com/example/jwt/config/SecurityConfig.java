bed224df7957082ba9597bf644e8609d5cf36278
sinryuji <siler2474@gmail.com>
2023-08-12 18:20:09 +0900

    security setting

---
+++ src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.jwt.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsFilter corsFilter;
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .addFilter(corsFilter) // @CrossOrigin(인증 없을때 사용), 시큐리티 필터에 등록 인증(O)
+            .formLogin().disable()
+            .httpBasic().disable()
+            .authorizeHttpRequests()
+            .antMatchers("/api/v1/user/**").hasAnyRole("USER", "MANAGER", "ADMIN")
+            .antMatchers("/api/v1/manager/**").hasAnyRole("MANAGER", "ADMIN")
+            .antMatchers("/api/v1/admin/**").hasAnyRole("ADMIN")
+            .and().build();
+    }
+}
