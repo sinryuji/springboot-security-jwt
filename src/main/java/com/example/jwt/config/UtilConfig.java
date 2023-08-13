67bb73cac28558b8d0c4e352dac9d5826c702365
sinryuji <siler2474@gmail.com>
2023-08-13 23:33:37 +0900

    회원가입

---
+++ src/main/java/com/example/jwt/config/UtilConfig.java
@@ -0,0 +1,15 @@
+package com.example.jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class UtilConfig {
+
+    // 해당 메서드의 리턴되는 오브젝트를 IOC로 등록 해준다.
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/example/jwt/controller/RestApiController.java
+++ src/main/java/com/example/jwt/controller/RestApiController.java
@@ -1,11 +1,21 @@
 package com.example.jwt.controller;
 
+import com.example.jwt.entitiy.User;
+import com.example.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class RestApiController {
+
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping("/")
     public String home() {
@@ -15,5 +25,15 @@ public class RestApiController {
     @PostMapping("/")
     public String token() {
         return "<h1>token</h1>";
+    }
+
+    @PostMapping("/join")
+    public String join(@RequestBody User user) {
+        user.setRoles("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user); // 그냥 이대로 하면 시큐리티로 로그인 할 수 없음. 이유는 패스워드가 암호화가 안되어있기 때문.
+        return "회원가입 완료";
     }
 }
